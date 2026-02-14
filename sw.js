3db9ad92b86020f29a630eef71be1b3d8dff8359
zxc8011097 <954234885@qq.com>
2026-02-14 12:59:30 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,22 @@
-const CACHE_NAME = 'football-v1';
-const urlsToCache = ['/','/index.html','/manifest.json','/icons/icon-192.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE_NAME = 'football-predict-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
